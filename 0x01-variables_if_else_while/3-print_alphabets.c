a18d517ebb4871c4614dc2ed87456853227e2674
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-09 15:00:02 -0700

    General commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
  /**
-  * *Description: main - prints lower case then upper followed by new line.
+  *Description: main - prints lower case then upper followed by new line.
   *
   * *Return: 0 if functional
-  * */
-int main()
+  */
+int main(void)
 
 {
-
-	  	char ch;
-
-	  	for(ch = 'a'; ch <= 'z'; ch++)
+		char ch;
+		
+		for(ch = 'a'; ch <= 'z'; ch++)
 		{
 			putchar(ch);
 		}
 		for (ch = 'A'; ch <= 'Z'; ch++)
 		{
-       			putchar(ch);
+			putchar(ch);
 		}
 		putchar('\n');
 		return 0;
