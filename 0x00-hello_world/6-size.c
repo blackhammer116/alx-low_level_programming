f1dd1cbaf9afe08e966b40a0b12ef0a3b39aeda3
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-09 08:22:32 -0700

    General commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *Description: main - will show size of DT.
+ *
+ * Return: 0 if functional
+ */
+int main(void)
+{
+printf("size of a char: %d byte(s)\n",sizeof(char);
+printf("size of an int: %d byte(s)\n",sizeof(int));
+printf("size of a long int: %d byte(s)\n",sizeof(long int)0);
+printf("size of a long long int: %d byte(s)\n",sizeof(long long int));
+printf("size of a float: %d byte(s)\n",sizeof(float)0);
+return (0);
+}
