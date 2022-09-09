1f2ca426c1e0eb0ce110605b580a6cdb8f9c5f67
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-09 15:02:25 -0700

    General commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 {
 		char ch;
 		
-		for(ch = 'a'; ch <= 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			putchar(ch);
 		}
@@ -18,5 +18,5 @@ int main(void)
 			putchar(ch);
 		}
 		putchar('\n');
-		return 0;
+		return (0);
 }
