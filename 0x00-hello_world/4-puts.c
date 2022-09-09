5586a41ec7b77974efc5eb2355373802c0b05d9c
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-09 07:41:43 -0700

    General commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+/*this will print out "Programming is like building a multilingual puzzle */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
