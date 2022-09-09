3037a092e00f78aee1918665c8fa0e5f9c5b31ab
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-09 07:57:37 -0700

    General commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
-/*Description:- The main function will print out 
- * \"Programming is like building a multilingual puzzle 
- * and then return 0 if the code is functional
+/**
+ * Description: main - will print out a string to stdout.
+ *
+ * Return: 0 if the code is functional.
 */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
