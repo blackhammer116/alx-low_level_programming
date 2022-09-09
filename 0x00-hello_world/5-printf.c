666a40746195dd56252fc0385ef76163c055dc61
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-09 08:09:19 -0700

    General commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *Description: main will print a string onto stdout using printf.
+ *Description: main - will print a string onto stdout using printf.
  *
  * Return: 0 if functional.
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
