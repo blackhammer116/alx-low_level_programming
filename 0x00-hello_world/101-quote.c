5a3fb6cb41807d1450217853c3abc39c846e318d
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-09 10:46:59 -0700

    General commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *Description: main - is supposed to print out string to stderr.
+ *Description: main - is supposed to print stderr.
  *
  *Return: 1 if functional.
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fputs(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
