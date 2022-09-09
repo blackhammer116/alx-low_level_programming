ef9e4cc2a0acfcb758c36ee4f42692f23c46af9a
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-09 08:04:48 -0700

    General commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *Description: main will print a string onto stdout using printf.
+ *
+ * Return: 0 if functional.
+ */
+int main(void);
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
