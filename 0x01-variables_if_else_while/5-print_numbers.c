b179afff9585a3bcfc09eeecda88b61f53c9aac0
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-12 08:43:11 -0700

    General commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Description: main - this function will print out numbers of base 10
+ *
+ * Return 0 if funtional
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10 ; num++)
+	{
+		printf("%d",num);
+	}
+	printf("\n");
+return (0);
+}
