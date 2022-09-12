93ae0f92f7950f9fdb08196fad9f23332afb5f1c
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-12 08:26:27 -0700

    General commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,14 +10,14 @@
 int main()
 {
 	char ch;
-	for (ch ='a'; ch <= 'z'; ch++)
-	{	
-	  if (ch == 'e' || ch == 'q')
-       {
-	       continue;
-       }
-	  putchar(ch);
-  }
+	for (ch = 'a' ;ch <= 'z' ; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+	{
+		continue;
+	}
+	putchar(ch);
+	}
 	putchar('\n');
-   return 0;
+return (0);
 }
