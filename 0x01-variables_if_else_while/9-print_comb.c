01d68dccbc185f3bbcfb1443a6b0f5515ff6ab65
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-12 09:54:17 -0700

    General commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,15 @@
 int main(void)
 {
 	int num;
-	for (num = '0'; num < '10';num++)
+
+	for (num = '0'; num <= '9';num++)
 	{
 		putchar(num);
-		putchar(',');
-		putchar(' ');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 return (0);
