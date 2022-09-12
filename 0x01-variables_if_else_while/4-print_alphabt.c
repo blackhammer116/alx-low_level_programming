7213eb7988cfd18a5e5076b24e96955d1a260e12
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-12 08:02:01 -0700

    General commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+/**
+ *Description: main - the functions print the alphabet except
+ *q and e
+ *
+ * *Return: 0 if functional
+ *
+ */
+int main()
+
+{
+	    char ch;
+
+        for (ch='a'; ch <= 'z'; ch++)
+  {
+          if (ch == 'e' || ch == 'q')
+       {
+          ch++;
+       }
+          else if (ch != 'e' || ch != 'q')
+       {
+          putchar(ch);
+       }
+       }
+   putchar("\n");
+   return 0;
+}
