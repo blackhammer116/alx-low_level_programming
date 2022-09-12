d9f508751a4989288b278b5225a8d0848a096eba
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-12 09:15:23 -0700

    General commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints letters in reverse order
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+return (0);
+}
