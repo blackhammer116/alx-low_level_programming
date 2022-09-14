fe23f31062f72fd87c40dd19af2eb997e7431be5
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-14 15:34:48 -0700

    prints till 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,11 +6,13 @@
  */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n == 98)
 		printf("%d",n);
 	else if (n > 98)
 	{
-		for (int i = n; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			printf("%d",i);
 			if (i != 98)
@@ -19,7 +21,7 @@ void print_to_98(int n)
 	}
 	else if (n < 98)
 	{
-		for (int i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
