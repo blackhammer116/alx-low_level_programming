0daa88f1147920478815e1d915ea374aaee947dc
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-14 06:01:44 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char name[] = "_putchar";
+	for (int i = 0; i <= 8; i++)
+	{
+	_putchar(name[i]);
+	}
+	_putchar('\n')
+return (0);
+}
