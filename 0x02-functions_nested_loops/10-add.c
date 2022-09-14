2feb39d433c89bebf11ed97fb05d034f317b3685
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-14 14:59:35 -0700

    adds tow numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *add - adds two numbers and prints output
+ *Return: sum
+ */
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+	return (sum);
+}
