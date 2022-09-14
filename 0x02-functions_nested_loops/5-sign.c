c97c6db5b7e6832b7cc35a979e0bc1010d24ebc1
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-14 11:55:23 -0700

    sign checker

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,14 +2,14 @@
 /**
  * print_sign - checks the sign of the given number
  * @n: is the parameter
- * return: 1 if positive,0 if zero, -1 if negative
+ * Return: 1 if positive,0 if zero, -1 if negative
  */
 int print_sign(int n)
 {
-	if (n >= 0)
-		return (1);
+	if (n > 0)
+		return (+1);
 	else if (n == 0)
 		return (0);
-	else
+	else if (n < 0)
 		return (-1);
 }
