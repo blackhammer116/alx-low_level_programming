a2466917153d67f89525ffdb0e10b61448454310
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-14 10:44:37 -0700

    header file defintion

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,14 @@
  */
 int main(void)
 {
-	char name[8] = "_putchar";
-	for (int i = 0; i <= 7; i++)
-	{
-	_putchar(name[i]);
-	}
-	_putchar('\n')
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 return (0);
 }
