777b711b55c14e5f1047f3979f85ee5ff146e71b
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-14 11:38:41 -0700

    case checker

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 #include <ctype.h>
 /**
  *_islower - checks if letter is lower case
- *int c - is parameter of function
- * Return - 1 if lower 0 if not
+ *parameter: is parameter of function
+ * Return: 1 if lower 0 if not
  */
 int _islower(int c)
 {
