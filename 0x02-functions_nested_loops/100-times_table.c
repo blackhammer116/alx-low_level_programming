1a11db200c2fb481fd4e5461129f4f663803c0e9
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-14 15:52:19 -0700

    if and loop

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - multiplication table
+ */
+void print_times_table(int n)
+{
+	if (n !> 15 || n !< 0)
+	{
+		int i;
+		int j;
+		int x;
+
+		for (i = 0; i <= n + 1; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{	
+				x = i * j;
+				if (j == 0)
+					_putchar(x + '0');
+				if (x < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(x + '0');
+				}
+				else if (x >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((x / 10) + '0');
+					_putchar((x % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
