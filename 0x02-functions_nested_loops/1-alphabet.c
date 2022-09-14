2e77949772a17696df3db178277b533c53a8f3e5
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-14 11:03:44 -0700

    general commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - prints alphabet in lower
+ * print_alphabet - prints alphabet in lower
  * Return: 0
  */
 void print_alphabet(void)
 {
 	int alpha;
 
-	for ( alpha = 97; alpha <= 122; alpha++){
+	for (alpha = 97; alpha <= 122; alpha++)	
 		_putchar(alpha);
-	}
 	_putchar('\n');
 }
