e64fb4128fce1ec20e892067ba63e5b6947294ee
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-14 11:51:45 -0700

    sign checker

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_sign - checks the sign of the given number
+ * @n: is the parameter
+ * return: 1 if positive,0 if zero, -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n >= 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
