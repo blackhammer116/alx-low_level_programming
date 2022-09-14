eb869dd9d3775a8c6ae2a07dbb5fb33ea68ce5ae
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-14 16:06:13 -0700

    if and loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,14 +22,24 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar(x + '0');
 				}
 				else if (x >= 10)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar((x / 10) + '0');
 					_putchar((x % 10) + '0');
+				}
+				else if (k >=100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0'); 
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
