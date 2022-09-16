5617d7b53aafea4b016074964cb49007c0d6d6c8
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-15 19:48:35 -0700

    first debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "math.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Retrun: 0
+ */
+int main(void)
+{
+	int i;
+
+	i =0;
+	positive_or_negative(i);
+	return (0);
+}
