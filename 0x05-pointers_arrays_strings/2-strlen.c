4da8c14cdad8e35ee11bb9740ea4371d0442ab7a
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-19 10:15:16 -0700

    counting the number of strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - calculates the number of characters
+ * a string has
+ * @s: is the string parameter to be passed
+ *Return: strlen(s)
+ */
+int _strlen(char *s)
+{
+	strlen(s);
+	return (strlen(s));
+}
