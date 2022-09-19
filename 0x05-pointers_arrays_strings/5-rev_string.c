b33aab73a52de396f86d73ea78548e73c05ec676
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-19 11:49:02 -0700

    printing reversed string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *print_rev - prints string in reverse order
+ *@s: is the parameter to be passed
+ */
+void print_rev(char *s)
+{
+	strrev(s);
+
+	puts(s);
+}
