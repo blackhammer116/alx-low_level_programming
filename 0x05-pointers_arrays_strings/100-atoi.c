17488c70ca2a75589cd2e68bf5af9db7a4cfb366
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-20 11:33:03 -0700

    converting string into int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_atoi - is the function that changes string into an int
+ *@s: first parameter
+ *Return: 0
+ */
+int _atoi(char *s)
+{
+	int val = atoi(s);
+	return (0);
+}
