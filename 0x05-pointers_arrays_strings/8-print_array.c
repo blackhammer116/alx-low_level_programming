12999d09f5f067fb5ba242ecee7190d9a2a0d95b
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-20 10:58:47 -0700

    printing arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - print arrays in accordance to the given
+ * parameter
+ * @a: array variable to be passed
+ * @n: number of arrays to be printed
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (n != 0 && n != 1)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			if (i != n)
+				printf("%d, ", a[i]);
+			else if (i == n)
+				printf("%d", a[i]);
+		}
+	}
+	printf("\n");
+}
