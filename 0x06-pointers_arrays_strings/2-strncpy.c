597029d30b8369e8e98cc0180318a1ed13605ddd
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-22 08:30:58 -0700

    copying a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <string.h>
 /**
- *
+ *_strncpy - function copies src onto dest with the constraints of n
+ *@dest: first parameter
+ *@src: second parameter
+ *@n: constraint parameter
+ *Return: returns first parameter after copying.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
