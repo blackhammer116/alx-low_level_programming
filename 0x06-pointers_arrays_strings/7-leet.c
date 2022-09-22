9985a31274d3b45903f7d2a9c3d7f8a43f66b2d9
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-22 11:04:22 -0700

    leet code

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *leet - function convertes a string into leet code
+ * @s: string parameter
+ * Return: s the string parameter
+ */
+char *leet(char *s)
+{
+	int x,i;
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0;s[i] != '\0'; i++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			if (s[i] == a[x])
+			{
+				a[i] = b[x];
+			}
+		}
+	}
+	return (s);
+}
