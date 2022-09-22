0724b11e9876ddc76a72c6d4154cfa71f9eb7270
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-22 11:50:31 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *rot13 - function converts string into rot13 code
+ * @s: first string parameter
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}	
+		}
+	}
+	return (s);
+}
