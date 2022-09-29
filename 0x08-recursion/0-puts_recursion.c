22d2a8b229392d64dd00d17f4f232a71cee2e30a
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-29 02:34:36 -0700

    recursion in string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string using recursion method
+ * @s: string parameter
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		putchar('\n');
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
