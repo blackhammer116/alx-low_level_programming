351a0e40606086e914cbdaad7beb7384da6122f0
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-29 11:31:47 -0700

    checking if string is palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,6 +10,7 @@ int is_palindrome(char *s)
 {
 	if (*s == '0')
 		return (1);
+	
 	return (check_palindrome(s));
 }
 /**
@@ -20,14 +21,16 @@ int is_palindrome(char *s)
 int check_palindrome(char *s)
 {
 	int l = _strlen_recursion(s) - 1;
-
+	
 	if (*s == s[l])
 	{
 		s++;
 		l--;
 	}
 	else
+	{
 		return (0);
+	}
 	return (1);
 }
 /**
