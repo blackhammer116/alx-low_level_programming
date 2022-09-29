a64547b063e4ec1d2ddd52f56d9de5749c50f52e
Nebiyu <nebiyuhiyabu@gmail.com>
2022-09-29 08:30:03 -0700

    calculating power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _pow_recursion - calculates power of given number 
+ * _pow_recursion - calculates power of given number
  * with base and exponent
  * @x: base number
  * @y: exponent
  * Return: if y < 0, -1 as an error else the power of the
- * numbers 
+ * numbers
  */
 int _pow_recursion(int x, int y)
 {
