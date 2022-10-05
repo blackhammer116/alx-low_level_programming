f50d5129d7bcd895bc2eafa4377b4982718c2cc6
Nebiyu <nebiyuhiyabu@gmail.com>
2022-10-05 02:10:01 -0700

    allocating the same memory as a given string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * str_concat - allocates memory and concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: NULL if it fails the pointer a if success
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 1, j = 0;
+	char *a, b;
+
+	b = strcat(s1, s2);
+	while (b[i])
+	{
+		i++;
+	}
+	a = malloc((sizeof(char) * i) + 1);
+	if (a == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		a[j] = b[j];
+		j++;
+	}
+	a[j] = '\0';
+	return (a);
+}
