60bec3b7fa8db748a02bad58454687e35e031289
Nebiyu <nebiyuhiyabu@gmail.com>
2022-10-05 00:59:26 -0700

    creating character array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of dynamic size
+ * @size: size of the array
+ * @c: character to be set
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *a;
+
+	if (size == 0)
+		return (NULL);
+	s = malloc(size * sizeof(char));
+	if (size == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
+}
