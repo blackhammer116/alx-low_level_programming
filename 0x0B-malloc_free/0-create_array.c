39bf78dbc7000743bafdcfaf0d8f59374683378a
Nebiyu <nebiyuhiyabu@gmail.com>
2022-10-05 01:03:35 -0700

    creating character array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	a = malloc(size * sizeof(char));
-	if (size == NULL)
+	if (size == '\0')
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
