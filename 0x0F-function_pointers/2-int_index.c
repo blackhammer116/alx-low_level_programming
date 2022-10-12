f6ea529693c72fc64b2c08d2a510bf8d47a5f1f5
Nebiyu <nebiyuhiyabu@gmail.com>
2022-10-12 01:04:03 -0700

    simple search

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - searches the given number through an array
+ * and returns the index number if found
+ * @array: array parameter
+ * @size: size of array
+ * @cmp: pointer to a function containing the number to be searched
+ * Return: -1 on error the index number on success
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+	int j = 0;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		j = cmp(array[i]);
+		if (j != 0)
+			return (i);
+	}
+	return (-1);
+}
