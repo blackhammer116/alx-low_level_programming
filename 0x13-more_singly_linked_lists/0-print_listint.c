1222dd8d9c2ac44990ad283301d59f18e9d3fc13
Nebiyu <nebiyuhiyabu@gmail.com>
2022-10-25 03:17:05 -0700

    printing all elements in the list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - prints elements of a list
+ * @h: list pointer
+ * Return: the number of nodes "i"
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
