b0f6bf3494d800fda5f2787bfb9c598c38b3dc27
Nebiyu <nebiyuhiyabu@gmail.com>
2022-10-25 22:59:25 -0700

    bug fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,19 +4,17 @@
  * @head: list pointer
  */
 
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
 	if (head == NULL)
 		return;
-	while (head)
+	while (*head != NULL)
 	{
-		temp = head->next;
-		free(head->n);
-		free(head);
-		head = temp;
-
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
 	}
 	*head = NULL;
 }
