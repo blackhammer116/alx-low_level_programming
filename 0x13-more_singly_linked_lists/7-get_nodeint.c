00be03cfdcf7c65ad4ae50642a6d96840b39c12c
Nebiyu <nebiyuhiyabu@gmail.com>
2022-10-26 01:26:23 -0700

    getting a data from a list using index positions

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,11 +12,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	listint_t *temp;
 
+	if (head == NULL)
+		return (NULL);
+
 	temp = head;
 	while (temp != NULL)
 	{
 		if (c == index)
-			return (temp->n);
+			return (temp);
 		c++;
 		temp = temp->next;
 	}
