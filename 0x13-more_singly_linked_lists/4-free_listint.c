77131bc26527b44d79db71f3be3cdce1ed0f5690
Nebiyu <nebiyuhiyabu@gmail.com>
2022-10-25 22:55:39 -0700

    bug fixed

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,10 +10,8 @@ void free_listint(listint_t *head)
 
 	while (head)
 	{
-		temp = head->next;
-		free(head->n);
-		free(head);
-		head = temp;
-
+		temp = head;
+		head = head->next;
+		free(temp);
 	}
 }
