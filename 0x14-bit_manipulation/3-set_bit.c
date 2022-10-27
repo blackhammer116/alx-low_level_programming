8c1488cbb8108bd9a75ce60fef054184b4da85b8
Nebiyu <nebiyuhiyabu@gmail.com>
2022-10-27 10:48:57 -0700

    setting a bit value of a number to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
- *
+ * set_bit - sets a bit value of a number to 1
+ * @n: number that gets changed
+ * @index: index value
+ * Return: -1 on failure, 1 on success
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
