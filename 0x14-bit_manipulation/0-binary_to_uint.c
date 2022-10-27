5cc1f46bd39221bb224e925857a2dcdb663fb71d
Nebiyu <nebiyuhiyabu@gmail.com>
2022-10-27 10:01:52 -0700

    converting binary into an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	while (b[i])
 	{
-		if (b[i] == 0 || b[i] == 1)
+		if (b[i] == '0' || b[i] == '1')
 		{
 			c <<= 1;
 			c += b[i] + '0';
