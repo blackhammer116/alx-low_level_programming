8fef38fb8dd4a58116e359ed83c61e9d62757e45
Nebiyu <nebiyuhiyabu@gmail.com>
2022-11-01 12:14:16 -0700

    copying one file into another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,26 +11,27 @@ void close(char **BUF, int *fd1, int *fd2);
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, *fd1 = &fd_from, *fd2 = &fd_to;
-	int j, i;
-	char *buf = malloc(sizeof(char) * 1024), **BUF = &buf;
+	int j, i, n = 0;
+	char *buf, **BUF = &buf;
 
 	if (argc != 3)
 	{
 		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY, 0600);
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_CREAT | O_WRONLY, 0664);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
+	buf = malloc(sizeof(char) * 1024);
 	do {
 		i = read(fd_from, buf, 1024);
 		if (i == -1)
@@ -44,6 +45,7 @@ int main(int argc, char *argv[])
 			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
+		n++;
 	} while (i == 1024);
 	close(BUF, fd1, fd2);
 
