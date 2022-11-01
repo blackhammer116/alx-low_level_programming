54ddd39d61efeb12241be7a73a12cfab3aff9e2a
Nebiyu <nebiyuhiyabu@gmail.com>
2022-11-01 02:44:30 -0700

    creating a new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,10 +10,10 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, i = 0, j;
-	
+
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
 
 	if (fd == -1)
 		return (-1);
@@ -22,7 +22,6 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	
 	while (text_content[i])
 		i++;
 
