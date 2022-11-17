93066fed5ff65c1266dceab420020a82bcf97542
Nebiyu <nebiyuhiyabu@gmail.com>
2022-11-17 10:04:59 -0800

    printing numbers with nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0 to 14 10 times
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 10; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
