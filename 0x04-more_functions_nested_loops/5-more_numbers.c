a56cf5e45ca47133bbdd4c778cfc6710448e98cc
Nebiyu <nebiyuhiyabu@gmail.com>
2022-11-17 10:08:08 -0800

    printing numbers with nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 10; i <= 10; i++)
+	for (i = 10; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j > = 10)
+				_putchar ((j / 10) + '0');
+
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
