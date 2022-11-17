5ce70825cc137633ed34b5946df287d852ca8607
Nebiyu <nebiyuhiyabu@gmail.com>
2022-11-17 11:38:06 -0800

    FizzBuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - FizzBuzz check
+ * Return: 0
+ */
+int main(void)
+{
+	int  i = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) ! = 0)
+			printf("Fizz");
+		else if ((i % 5) == 0 && (i % 3) != 0 && i != 100)
+			printf("Buzz");
+		else if ((i % 5) == 0 && (i % 3) != 0 && i == 100)
+			printf("Buzz");
+		else if ((i % 5) == 0 && (i % 3 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
