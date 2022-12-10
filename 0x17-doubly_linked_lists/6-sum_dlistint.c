cc76ba808d22f8350b722cf236bda4645e2566ba
Nebiyu <nebiyuhiyabu@gmail.com>
2022-12-09 18:20:09 -0800

    adding the datas in a list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sums up all the data in the list
+ * @head: head pointer
+ * Return: the sum of the datas in the list and 0
+ * if th list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	int sum;
+	if (!head)
+		return (0);
+	if (!head->next)
+		return head->n;
+	while (temp)
+	{
+		sum  += temp->n;
+		temp->next;
+	}
+	return (sum);
+}
