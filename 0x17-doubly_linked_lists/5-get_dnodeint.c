7f3f11ade3a279e4b4d445cab1a9b6feae6de2f6
Nebiyu <nebiyuhiyabu@gmail.com>
2022-12-09 17:58:10 -0800

    getting a node at a specific position

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - gets a node from the list at
+ * a given position
+ * @head: head pointer
+ * @index: index of the required node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp;
+	int i = 0;
+
+	if (!head)
+		return (NULL);
+	if (i == index)
+		return head;
+	while (temp)
+	{
+		if (i == index)
+			return (temp);
+		i++;
+		temp = temp->next;
+	}
+	return (NULL);
+}
