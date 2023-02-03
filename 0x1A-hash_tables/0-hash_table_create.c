3cb08884e3715395149ec883e9559399d1f8fbbd
Nebiyu <nebiyuhiyabu@gmail.com>
2023-02-03 02:44:53 -0800

    creating a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = (hash_table_t*) malloc(sizeof(hash_table_t));
+	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
 
 	table->size = size;
 
