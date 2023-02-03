b432c0ac15ae4d737c70584003d8ab51139b4e56
Nebiyu <nebiyuhiyabu@gmail.com>
2023-02-03 04:33:41 -0800

    djb2 algorithm

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function that returns the index of a key
+ * @key: key to be check
+ * @size: size of the array
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+{
+	unsigned long int hash = hash_djb2((unsigned char *) key);
+	unsigned long int key_index = hash % size;
+
+	return (key_index);
+}
